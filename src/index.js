2563bbb62972b4d052a9cc54de5574447d1ec750
Piotr Bania <piotr.bania@europe.com>
2022-03-21 20:26:35 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as THREE from '../src/modules/three.module.js'
+import { gsap } from '../node_modules/gsap/gsap-core.js'
+import { OrbitControls } from '../src/modules/OrbitControls.js'
+import { GUI } from '../node_modules/dat.gui/build/dat.gui.module.js'
+import { RGBELoader } from '../src/modules/RGBELoader.js'
+
+console.log(THREE)
+console.log(gsap)
+console.log(OrbitControls)
+console.log(GUI)
+console.log(RGBELoader)
